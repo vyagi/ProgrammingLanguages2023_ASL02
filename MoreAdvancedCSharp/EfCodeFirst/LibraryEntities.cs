a14c2107dffdf1857ad721cb5da3680968eb37a9
vyagi <marcin.jagiela@hotmail.com>
2023-05-25 18:49:24 +0200

    Ef code first done

---
+++ MoreAdvancedCSharp/EfCodeFirst/LibraryEntities.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfCodeFirst;
+
+public class LibraryEntities : DbContext
+{
+    public DbSet<Book> Books { get; set; }
+
+    public DbSet<Author> Authors { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => 
+        optionsBuilder.UseSqlServer("Data Source=localhost,1433;Initial Catalog=Library;User ID=sa;Password=password-1234;Encrypt=false");
+}
